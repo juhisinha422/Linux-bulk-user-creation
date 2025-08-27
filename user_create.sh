4d6bc4caa9502ce8dde29abd8e62a82d01260767
Juhi Sinha <46685880+juhisinha422@users.noreply.github.com>
2025-08-27 14:51:25 +0530

    Update user_create.sh

--- user_create.sh
+++ user_create.sh
@@ -13,3 +13,19 @@ while read user; do
         echo "User $user created successfully." >> $LOG_FILE
     fi
 done < "$USER_FILE"
+
+
+
+𝗟𝗶𝗻𝘂𝘅 : 𝗕𝘂𝗹𝗸 𝗨𝘀𝗲𝗿 𝗖𝗿𝗲𝗮𝘁𝗶𝗼𝗻
+Built a simple bash script to create multiple users in seconds 🚀
+ • Reads usernames from a text file
+ • Sets default passwords
+ • Forces password reset at first login
+ • Prevents duplicates & logs all actions
+
+Why? - Saves manual work for sysadmins, dev teams, or training labs.
+GitHub: https://lnkd.in/dDmBGvhm
+
+Run it in 2 steps:
+chmod +x user_create.sh
+sudo ./user_create.sh
